e98a32e5dfe03331f67f46db4a3f5518503849ca
Almog Mizrahi <almogosh111@gmail.com>
2026-05-12 22:07:48 +0300

    add login ans singup route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -8,6 +8,7 @@ import { User } from '../models/User.js'
 
 const router = express.Router()
 
+// SINGUP ROUTE
 router.post('/signup', async (req, res) => {
     try {
         const { name, email, password } = req.body
@@ -65,3 +66,60 @@ router.post('/signup', async (req, res) => {
 })
 
 export default router
+
+// LOGIN ROUTE 
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body
+        
+        if (!email || !password) {
+            return res.status(400).json({
+                message: 'Email and password are required',
+            })
+        }
+        
+        const user = await User.findOne({ email })
+        
+        if (!user) {
+            return res.status(401).json({
+                message: 'Invalid credentials',
+            })
+        }
+        
+        const isMatch = await bcrypt.compare(
+            password,
+            user.password,
+        )
+        
+        if (!isMatch) {
+            return res.status(401).json({
+                message: 'Invalid credentials',
+            })
+        }
+        
+        const token = jwt.sign(
+            { userId: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' },
+        )
+        
+        res.status(200).json({
+            message: 'Login successful',
+            token,
+            
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                isOnboardingCompleted:
+                user.isOnboardingCompleted,
+            },
+        })
+    } catch (err) {
+        console.log(err)
+        
+        res.status(500).json({
+            message: 'Server error',
+        })
+    }
+})
