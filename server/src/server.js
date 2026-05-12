2826216fc2b2988b7553123e45bada8d6f70c3aa
Almog Mizrahi <almogosh111@gmail.com>
2026-05-12 23:53:58 +0300

    add onboarding preferences backend

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js'
 import authRoutes from './routes/auth.routes.js'
+import onboardingRoutes from './routes/onboarding.routes.js'
 
 dotenv.config()
 
@@ -15,6 +16,7 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRoutes)
+app.use('/api/onboarding', onboardingRoutes)
 
 app.get('/', (req, res) => {
     res.send('API is running...')
