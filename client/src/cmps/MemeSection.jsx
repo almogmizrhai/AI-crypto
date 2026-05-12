79db12c296a859a426391de2bcea2d0022c9b202
Almog Mizrahi <almogosh111@gmail.com>
2026-05-13 01:49:14 +0300

    add crypto meme

--- client/src/cmps/MemeSection.jsx
+++ client/src/cmps/MemeSection.jsx
@@ -1,12 +1,36 @@
 // MemeSection cmp jsx
 
+import { useEffect, useState } from 'react'
+
+import { getCryptoMeme } from '../services/dashboard.service.js'
+
 export function MemeSection() {
+    const [meme, setMeme] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    
+    async function loadMeme() {
+        try {
+            const data = await getCryptoMeme()
+            setMeme(data)
+        } catch (err) {
+            console.log(err)
+        } finally {
+            setIsLoading(false)
+        }
+    }
+    useEffect(() => {
+        loadMeme()
+    }, [])
+    
+    if (isLoading) return <p>Loading meme...</p>
+
     return (
-    <section className="dashboard-section">
-        <h2>Fun Meme</h2>
-        <div className="dashboard-card">
-            Meme will appear here 
-            </div>
+    <section className="meme-section dashboard-section">
+        <h2>Fun Crypto Meme 😂</h2>
+        <div className="meme-card">
+            <p>{meme.title}</p>
+            <img src={meme.imageUrl} alt={meme.title} />
+        </div>
     </section>
     )
 }
