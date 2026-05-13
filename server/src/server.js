22eb800507f8c3381ef5a05018a606707eb4f596
Almog Mizrahi <almogosh111@gmail.com>
2026-05-13 22:54:07 +0300

    prepare for Render deployment

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,9 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 import { connectDB } from './config/db.js'
 
 import authRoutes from './routes/auth.routes.js'
@@ -18,13 +21,21 @@ connectDB()
 
 app.use(cors())
 app.use(express.json())
+
 app.use('/api/auth', authRoutes)
 app.use('/api/onboarding', onboardingRoutes)
 app.use('/api/votes', voteRoutes)
 app.use('/api/ai', aiRoutes)
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const clientDistPath = path.join(__dirname, '../../client/dist')
+
+app.use(express.static(clientDistPath))
+
+app.use((req, res) => {
+    res.sendFile(path.join(clientDistPath, 'index.html'))
 })
 
 const PORT = process.env.PORT || 3030
@@ -32,3 +43,4 @@ const PORT = process.env.PORT || 3030
 app.listen(PORT , () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
